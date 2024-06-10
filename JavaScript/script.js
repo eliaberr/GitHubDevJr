f5b26a0324251f56939524895397cb837f168044
eliaberr <eliabe859@gmail.com>
2024-06-10 08:53:11 -0300

    finalizado o desafio

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,27 +3,35 @@ function procurar(){
     var user = document.getElementById("nameUser").value
 
     var url = `https://api.github.com/users/${user}`
-    
+
     //coletando dados
-    nameUser = document.getElementById("name")
+    procurarId(nameUser, "name")
     htmlUser = document.getElementById("html_url")
     companyUser = document.getElementById("company")
     mensegerErro = document.getElementById("erro")
 
     $.getJSON(url, (user) => {
-        mensegerErro.innerHTML = ""
+        limpar(mensegerErro)
 
         nameUser.innerHTML = user.name;
         htmlUser.innerHTML = user.html_url;
         companyUser.innerHTML = 
         `<img src="${user.avatar_url}" class="shadow rounded mt-1" width="200">`
     }).fail(() => {
-        nameUser.innerHTML = ""
-        htmlUser.innerHTML = ""
-        companyUser.innerHTML = ""
-
+        limpar(nameUser)
+        limpar(htmlUser)
+        limpar(companyUser)
+        
         mensegerErro.innerHTML = `<div class="alert alert-danger" role="alert">
         Usúario Não Encontrado!
         </div>`
     })
+}
+
+function limpar(limpar){
+    limpar.innerHTML = ""
+}
+
+function procurarId(nomeVar, id){
+    nomeVar = document.getElementById($["id"])
 }
